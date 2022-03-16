4e2c4dfd571b16e5c1e533948fdc6fe442256c6a
Hawigirmachew <88828065+Hawigirmachew@users.noreply.github.com>
2022-03-16 15:58:53 -0700

    Update runner-up.py

--- runner-up.py
+++ runner-up.py
@@ -2,5 +2,11 @@ if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
     arr.sort(reverse=True)
-    print(arr)
+    y=-90909090
+    for i in range(len(arr)):
+        if arr[i]< max(arr):
+            y=arr[i]
+            break
+         
+    print(y)
     
